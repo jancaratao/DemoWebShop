057e60c1c827cd0267aa131cfd8918ab0831c608
jan.mark.p.caratao <jan.mark.p.caratao@CBEN-93WY3D3-LX.dir.svc.accenture.com>
2023-09-29 21:54:20 +0800

    commit

---
+++ Test Suites/visual test gift card.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>visual test gift card</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>21b2d843-6ed6-49ab-87af-bd0876f6a7c7</testSuiteGuid>
+   <testCaseLink>
+      <guid>6f289b2e-f9ff-4a5d-b452-3d8a7eae3312</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Categories - Gift Cards</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
