04014a619dc0fbb3cefd9c9e15baa915e9e4ae41
jan.mark.p.caratao <jan.mark.p.caratao@CBEN-93WY3D3-LX.dir.svc.accenture.com>
2023-09-29 21:00:55 +0800

    Commit

---
+++ Test Suites/Visual Test.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Visual Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e9cb7017-4f4a-4165-b2df-ce5ab442baac</testSuiteGuid>
+   <testCaseLink>
+      <guid>8dec0b3a-3919-4834-b4a0-7e8a7f03fb1f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Categories - Apparels and Shoes</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
